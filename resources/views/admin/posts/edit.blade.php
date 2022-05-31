e305366d6c34b6771ad1d91f75c8ad23307fd5d4
giorgios17 <giosur@live.it>
2022-05-31 15:09:13 +0200

    edit post creato

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,13 +2,21 @@
 @section('content')
     <form action="{{ route('admin.posts.update', $post->id) }}" method="post">
         @csrf
+        @method('PUT')
         <div class="form-group">
             <label for="title">Titolo</label>
-            <input type="text" class="form-control" name="title" id="title" placeholder="{{ old('title', $post->title) }}">
+            <input type="text" class="form-control" name="title" id="title" placeholder="Scrivi un titolo..."
+                value="{{ old('title', $post->title) }}">
+            @error('title')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label for="content">Contenuto</label>
-            <textarea class="form-control" name="content" id="content" rows="3"></textarea>
+            <textarea class="form-control" name="content" id="content" rows="3">{{ old('content', $post->content) }}</textarea>
+            @error('content')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <input class="btn btn-primary" type="submit">
     </form>
