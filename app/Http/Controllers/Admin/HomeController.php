15e87ae1caee5ba2beb3fee88bd561fb76c9059a
giorgios17 <giosur@live.it>
2022-05-31 09:46:42 +0200

    fix

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Post;
 class HomeController extends Controller
 {
     /**
@@ -16,9 +15,7 @@ class HomeController extends Controller
     public function index()
     {
         //
-        $posts = Post::all();
-
-        return view('admin.home', compact('posts'));
+        return view('admin.home');
     }
 
     /**
