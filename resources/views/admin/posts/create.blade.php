23aedd95b0d0e7fa0814173ace76ebc8f1b390f1
giorgios17 <giosur@live.it>
2022-05-31 14:47:24 +0200

    rimosso commento

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,25 +1,5 @@
 @extends('layouts.dashboard')
 @section('content')
-    {{-- <div class="form-container">
-        <form action="{{ route('admin.posts.store') }}" method="post">
-            @csrf
-            <label for="title">Titolo:</label>
-            <input type="text" name="title" id="title" value=""><br>
-            <label for="description">Descrizione:</label>
-            <input type="text" name="description" id="description" value=""><br>
-            <label for="thumb">Url immagine:</label>
-            <input type="text" name="thumb" id="thumb" value=""><br>
-            <label for="price">Prezzo:</label>
-            <input type="number" name="price" id="price" value=""><br>
-            <label for="series">Serie:</label>
-            <input type="text" name="series" id="series" value=""><br>
-            <label for="sale_date">Data di vendita:</label>
-            <input type="date" name="sale_date" id="sale_date" value=""><br>
-            <label for="type">Genere:</label>
-            <input type="text" name="type" id="type" value=""><br>
-            <input type="submit">
-        </form>
-    </div> --}}
     <form action="{{ route('admin.posts.store') }}" method="post">
         @csrf
         <div class="form-group">
